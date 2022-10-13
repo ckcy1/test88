981dc8951d8fbd9c5f91ac52bd4e7c03e53dd7c7
andy <ckcy1@qq.com>
2022-10-13 16:28:12 +0800

    0

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+trait test {
+    fn dushu(){
+        println!("dus");
+    }
+}
+struct dog{
+    name:String,
+}
+struct cat{
+    name:String,
+}
+impl test for dog{
+    fn dushu(){
+        println!("wwwwww");
+    }
+}
+
 fn main() {
     println!("test81");
 }
